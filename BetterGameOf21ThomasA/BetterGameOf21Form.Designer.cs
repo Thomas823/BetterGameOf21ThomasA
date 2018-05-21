19002f74557d8a173c1d5f9991ae8ca611d2733d
Thomas823 <35612259+Thomas823@users.noreply.github.com>
2018-05-21 09:57:31 -0400

    Initial commit

---
+++ BetterGameOf21ThomasA/BetterGameOf21Form.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace BetterGameOf21ThomasA
+{
+    partial class frmBetterGameOf21
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmBetterGameOf21
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "frmBetterGameOf21";
+            this.Text = "A Better Game Of 21 By Thomas Aubin";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ BetterGameOf21ThomasA/BetterGameOf21Form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BetterGameOf21ThomasA
+{
+    public partial class frmBetterGameOf21 : Form
+    {
+        public frmBetterGameOf21()
+        {
+            InitializeComponent();
+        }
+    }
+}
